3ebcff3482fe52e0f8cee94b2b552e704f4e847f
borealink <80372479+borealink@users.noreply.github.com>
2026-05-18 18:24:25 -0600

    Add files via upload

---
+++ website-starter-grom/website-starter-grom/core/database/database.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+// Database wraps the GORM database instance.
+type Database struct {
+	db *gorm.DB
+}
+
+// NewDatabase creates and initializes a new SQLite database connection.
+func NewDatabase(
+	path string,
+) (*Database, error) {
+	// Open the SQLite database using GORM.
+	db, err := gorm.Open(
+		sqlite.Open(path),
+		&gorm.Config{},
+	)
+
+	// Return error if connection fails.
+	if err != nil {
+		return nil, err
+	}
+
+	// Return the database wrapper instance.
+	return &Database{
+		db: db,
+	}, nil
+}
+
+// Get returns the underlying GORM database instance.
+func (database *Database) Get() *gorm.DB {
+	return database.db
+}
